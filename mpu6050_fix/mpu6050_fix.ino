aea42464ebbcdcf3e905ae7bbd8cbf9466edc988
Abdul Azzam Ajhari <42313402+abdazzamajhari@users.noreply.github.com>
2021-01-25 19:24:05 +0700

    update plus temperature

--- mpu6050_fix/mpu6050_fix.ino
+++ mpu6050_fix/mpu6050_fix.ino
@@ -100,6 +100,7 @@ void loop() {
                           ("&gx=") + Gx +
                           ("&gy=") + Gy +
                           ("&gz=") + Gz +
+                          ("&temp=") + T +
                           " HTTP/1.1\r\n" +
                  "Host: " + host + "\r\n" +
                  "Connection: close\r\n\r\n");
